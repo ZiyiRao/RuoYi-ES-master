c6946275ea61ddc6c84fdecd3d4f00c8618e3f90
Admin <2458654830@qq.com>
2022-11-30 11:22:13 +0800

    2

--- RuoYi/ruoyi-es/src/main/java/com/ruoyi/es/controller/EsController.java
+++ RuoYi/ruoyi-es/src/main/java/com/ruoyi/es/controller/EsController.java
@@ -3,42 +3,32 @@ package com.ruoyi.es.controller;
 import com.alibaba.fastjson.JSON;
 
 import net.sf.json.JSONArray;
-import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.es.bean.es.EsUserBean;
 import com.ruoyi.es.bean.es.EsNginxBean;
-import com.ruoyi.es.bean.es.ESUser;
-import com.ruoyi.es.bean.mysql.TsBean;
 import com.ruoyi.es.bean.mysql.UserBean;
 import com.ruoyi.es.service.EsService;
 import com.ruoyi.es.service.TsService;
 import com.ruoyi.es.service.UserService;
 import com.ruoyi.common.core.controller.BaseController;
-import com.ruoyi.es.utils.PageUtil;
-import com.ruoyi.system.domain.SysUser;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
 
 //import com.ruoyi.es.bean.es.Page;
-import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 /**
@@ -183,81 +173,33 @@ public class EsController extends BaseController {
      * @param keyword
      * @return
      */
-    @GetMapping("/searchField/{field}/{keyword}")
-    public String searchField( @PathVariable("field")String field,@PathVariable("keyword")String keyword,ModelMap mmap){
+    @GetMapping("/searchIpsField/{field}/{keyword}")
+    public String searchIpsField( @PathVariable("field")String field,@PathVariable("keyword")String keyword,ModelMap mmap){
         List<String> resultList = new LinkedList();
         resultList.add(field);
         resultList.add(keyword);
         JSONArray object = JSONArray.fromObject(resultList);
         System.out.println(object);
         mmap.put("list", object);
-        return "es/es_search";
-        //        RestHighLevelClient client = new RestHighLevelClient(
-//                RestClient.builder(
-//                        new HttpHost("localhost", 9200, "http"),
-//                        new HttpHost("localhost", 9201, "http")));
-//        List<EsUserBean> resultList = new LinkedList();		//返回的结果list
-//
-//        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder()     //创建 查询 对象
-//                .size(20);                         //返结果size
-////                .sort(new FieldSortBuilder("@timestamp")      //排序
-////                .order(SortOrder.DESC));                     //正序
-//        SearchRequest rq = new SearchRequest("net-ips22-2020.08.13")  //请求 索引库
-//                .source(sourceBuilder);						 //组装查询条件
-//        try {
-//            sourceBuilder.query(QueryBuilders
-//                    .boolQuery()
-//                    .must(QueryBuilders.wildcardQuery("Action",("*"+keyword+"*")))
-//                    .must(QueryBuilders.wildcardQuery("DstIP",("*"+keyword1+"*")))
-//
-//
-//            );
-//            rq.source(sourceBuilder);
-//            SearchResponse resp = client.search(rq, RequestOptions.DEFAULT);
-//            for (SearchHit hit : resp.getHits()) {
-//                com.alibaba.fastjson.JSONObject jsonObject = JSON.parseObject(hit.toString());
-//                String r = jsonObject.getString("_source");
-//                EsUserBean t = JSON.parseObject(r, EsUserBean.class);
-//                resultList.add(t);
-//            }
-//        }catch(Exception e){
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                client.close();    //关闭连接
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-////                JSONArray object2 = JSONArray.fromObject(resultList);
-////                String str=JSON.toJSON(resultList).toString();
-//        JSONArray object = JSONArray.fromObject(resultList);
-//                mmap.put("list", object);
-//                System.out.println(resultList);
-//                System.out.println("======================================================");
-////                System.out.println(str);
-////                System.out.println("======================================================");
-//                System.out.println(object);
-//
-//
-////                BoolQueryBuilder builder = QueryBuilders.boolQuery();
-////
-////                builder.should(QueryBuilders.matchPhraseQuery("IP",keyword));
-////                String s = builder.toString();
-////                System.out.println("es的数据查询 === "+s);
-////                Page<EsUserBean> search = (Page<EsUserBean>)esService.search(builder);
-////                List<EsUserBean> content = search.getContent();
-////                mmap.put("list", content);
-//            return "es/es_search";
-////                return getDataTable(object2);
-////        }
+        return "es/search_ips";
+
+    }
+
+    @GetMapping("/searchNginxField/{field}/{keyword}")
+    public String searchNginxField( @PathVariable("field")String field,@PathVariable("keyword")String keyword,ModelMap mmap){
+        List<String> resultList = new LinkedList();
+        resultList.add(field);
+        resultList.add(keyword);
+        JSONArray object = JSONArray.fromObject(resultList);
+        System.out.println(object);
+        mmap.put("list1", object);
+        return "es/search_nginx";
 
     }
 
     @ResponseBody
-    @PostMapping("/searchES/{field}/{keyword}")
-    public TableDataInfo searchES(@PathVariable("field")String field,@PathVariable("keyword")String keyword) {
+    @PostMapping("/searchEsIps/{field}/{keyword}")
+    public TableDataInfo searchEsIps(@PathVariable("field")String field,@PathVariable("keyword")String keyword) {
 //        startPage();
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
@@ -296,10 +238,50 @@ public class EsController extends BaseController {
         return getDataTable(object);
     }
 
+    @ResponseBody
+    @PostMapping("/searchEsNginx/{field}/{keyword}")
+    public TableDataInfo searchEsNginx(@PathVariable("field")String field,@PathVariable("keyword")String keyword) {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+        List<EsNginxBean> resultList = new LinkedList();		//返回的结果list
+
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder()     //创建 查询 对象
+                .size(20);                         //返结果size
+//                .sort(new FieldSortBuilder("@timestamp")      //排序
+//                .order(SortOrder.DESC));                     //正序
+        SearchRequest rq = new SearchRequest("nginx-client-2022.10")  //请求 索引库
+                .source(sourceBuilder);						 //组装查询条件
+        try {
+            sourceBuilder.query(QueryBuilders.matchQuery(field,keyword));
+//            sourceBuilder.query(QueryBuilders.matchAllQuery());
+            rq.source(sourceBuilder);
+            SearchResponse resp = client.search(rq, RequestOptions.DEFAULT);
+            for (SearchHit hit : resp.getHits()) {
+                com.alibaba.fastjson.JSONObject jsonObject = JSON.parseObject(hit.toString());
+                String r = jsonObject.getString("_source");
+                EsNginxBean t = JSON.parseObject(r, EsNginxBean.class);
+                resultList.add(t);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        } finally {
+            try {
+                client.close();    //关闭连接
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        JSONArray object = JSONArray.fromObject(resultList);
+        System.out.println(object);
+        return getDataTable(object);
+    }
+
 
     @ResponseBody
-    @GetMapping("/searchAll")
-    public List<EsUserBean> searchAll(){
+    @GetMapping("/searchAllIpsField")
+    public List<EsUserBean> searchAllIpsField(){
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost("localhost", 9200, "http"),
@@ -335,6 +317,46 @@ public class EsController extends BaseController {
         String str=JSON.toJSON(resultList).toString();
 //      System.out.println(object2);
         return object2;
+    }
+
+    @ResponseBody
+    @GetMapping("/searchAllNginxField")
+    public List<EsNginxBean> searchAllNginxField(){
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+        List<EsNginxBean> resultList = new ArrayList();		//返回的结果list
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder()      //创建 查询 对象
+                .size(1000)                              //返结果size
+                .sort(new FieldSortBuilder("@timestamp")      //排序
+                        .order(SortOrder.DESC));                     //正序
+        SearchRequest rq = new SearchRequest("nginx-client-2022.10")  //请求 索引库
+                .source(sourceBuilder);						 //组装查询条件
+        try {
+            sourceBuilder.query(QueryBuilders.matchAllQuery());
+            rq.source(sourceBuilder);
+            SearchResponse resp = client.search(rq, RequestOptions.DEFAULT);
+            for (SearchHit hit : resp.getHits()) {
+                com.alibaba.fastjson.JSONObject jsonObject = JSON.parseObject(hit.toString());
+                String r = jsonObject.getString("_source");
+                EsNginxBean t = JSON.parseObject(r, EsNginxBean.class);
+                resultList.add(t);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                client.close();    //关闭连接
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        JSONArray object = JSONArray.fromObject(resultList);
+        String str=JSON.toJSON(resultList).toString();
+//      System.out.println(object);
+        return object;
     }
 
 
